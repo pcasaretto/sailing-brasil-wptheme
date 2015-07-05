cfba9423ad964efabaf679b17545d8803b258758
Paulo L F Casaretto <pcasaretto@gmail.com>
2015-07-04 21:44:32 -0300

    Make some changes

--- header.php
+++ header.php
@@ -9,6 +9,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<link href='http://fonts.googleapis.com/css?family=Droid+Sans|Roboto+Condensed|Open+Sans' rel='stylesheet' type='text/css'>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
@@ -25,29 +26,29 @@
 <?php if((of_get_option('logo', true) != "") && (of_get_option('logo', true) != 1) ) { ?>
 			<h1 class="site-title logo-container"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
       <?php
-			echo "<img class='main_logo' src='".of_get_option('logo', true)."' title='".esc_attr(get_bloginfo( 'name','display' ) )."'></a></h1>";	
+			echo "<img class='main_logo' src='".of_get_option('logo', true)."' title='".esc_attr(get_bloginfo( 'name','display' ) )."'></a></h1>";
 			}
 		else { ?>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> 
-		<?php	
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<?php
 		}
 		?>
 		</div>
-        
+
         <?php get_template_part('social', 'sociocon'); ?>
-        
+
 	</header><!-- #masthead -->
-    
+
        <nav id="site-navigation" class="main-navigation" role="navigation">
          <div id="nav-container">
 			<h1 class="menu-toggle"><?php _e( 'Menu', 'pluto' ); ?></h1>
 			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'pluto' ); ?>"><?php _e( 'Skip to content', 'pluto' ); ?></a></div>
 
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-          </div>  
+          </div>
 		</nav><!-- #site-navigation -->
-        
+
 
 	<div id="content" class="site-content">
-	
+
     <?php get_template_part('slider', 'bx'); ?>
